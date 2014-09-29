f229e335055005e5b9066c962a999980f9e62a7a
Deniz Celik <deniz.celik@students.olin.edu>
2014-09-29 02:52:47 -0400

    ADDED: supporting files for deleting and editing usernames

---
+++ app/src/main/java/com/dcelik/myapplicationlistview/StringCallback.java
@@ -0,0 +1,9 @@
+package com.dcelik.myapplicationlistview;
+
+/**
+ * Created by dcelik on 9/29/14.
+ */
+public interface StringCallback {
+    public void handleString(String value);
+    public void handleDelete();
+}
